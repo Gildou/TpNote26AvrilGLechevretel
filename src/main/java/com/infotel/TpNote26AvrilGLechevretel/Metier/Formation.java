aa473245cccca9bc1f141784989a031c4771eab0
IN-MP-028 <javajee2k19@gmail.com>
2019-04-26 15:55:17 +0200

    commit initial

---
+++ src/main/java/com/infotel/TpNote26AvrilGLechevretel/Metier/Formation.java
@@ -0,0 +1,25 @@
+package com.infotel.TpNote26AvrilGLechevretel.Metier;
+
+public class Formation {
+	private int idFormation;
+	private String nomFormation;
+	public int getIdFormation() {
+		return idFormation;
+	}
+	public void setIdFormation(int idFormation) {
+		this.idFormation = idFormation;
+	}
+	public String getNomFormation() {
+		return nomFormation;
+	}
+	public void setNomFormation(String nomFormation) {
+		this.nomFormation = nomFormation;
+	}
+	@Override
+	public String toString() {
+		return "Formation [idFormation=" + idFormation + ", nomFormation=" + nomFormation + "]";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/infotel/TpNote26AvrilGLechevretel/Metier/package-info.java
@@ -0,0 +1,1 @@
+package com.infotel.TpNote26AvrilGLechevretel.Metier;
